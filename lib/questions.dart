edcccdfb0721304d337e1e8ba93b79b63280e022
nandanaajit <160300852+nandanaajit@users.noreply.github.com>
2024-02-20 16:08:39 +0530

    initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Quest extends StatefulWidget {
+  const Quest({Key? key}) : super(key: key);
+
+  @override
+  State<Quest> createState() => _QuestState();
+}
+
+class _QuestState extends State<Quest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreenAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('this is __________'),
+            add
+
+          ],
+        )
+      ),
+    );
+  }
+}
+
---
+++ lib/splash.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:quizapp/questions.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds:3), ()  {
+      Navigator.push(context, MaterialPageRoute(builder: (context) =>Quest() ));
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text('Quiz App'),
+        centerTitle: true,
+      ),
+
+
+      body: Center(
+          child: CircleAvatar(
+        radius: 100,
+        backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCiNg1rxaugA8XtUd2HnjLH4OA7auUNdPpQg&usqp=CAU'),
+      )
+      ),
+    );
+  }
+}
